29ad36f8d628c829b9bc133b341c42acd2e3715e
George Asiminei <george.asiminei@gmail.com>
2022-08-25 18:22:27 +0300

    remove browser implementation

--- browser/main.js
+++
@@ -1,8 +0,0 @@
-import { Provider, Signer, Transaction, SendMoney, Wallet } from "../dist/index.js";
-
-
-export {
-    Wallet, Provider, Signer, Transaction, SendMoney
-}
-
-window.GMD = { "Provider": Provider, "Signer": Signer, "Transaction": Transaction, "SendMoney": SendMoney, "Wallet": Wallet };
