cda0b2a3a17056fd3fa839c6433220219de16d5c
George Asiminei <george.asiminei@gmail.com>
2022-08-27 11:38:53 +0300

    replace node crypto module with window.crypto

--- lib/get-crypto.ts
+++ lib/get-crypto.ts
@@ -1,8 +1,9 @@
-/**
- * This file is required so it can be swapped by browserify with the browser equivalent at build time.
- * ./get-crypto.js is used in NodeJS and ./browser/get-crypto-window.js is used in browser.
- * Swapping is configured in packge.json
- */
 /* eslint @typescript-eslint/no-var-requires: "off" */
-import { webcrypto } from "crypto"
-export default webcrypto //Node equivalent of window.crypto in browser
+import { webcrypto } from "crypto" //Node equivalent of window.crypto in browser
+let cryptoExport: webcrypto.Crypto;
+if (webcrypto == undefined && window !== undefined) {
+    cryptoExport = window.crypto as webcrypto.Crypto;
+} else {
+    cryptoExport = webcrypto;
+}
+export default cryptoExport 
