f06e1704d15a96c584e6dbb38a12552f5e2a2d0f
George Asiminei <george.asiminei@gmail.com>
2022-08-31 23:31:41 +0300

    expot crypto util. needed for NQT<->GMD conversion

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,8 @@ import { Transaction } from "./transactions/transaction.js";
 import { SendMoney } from "./transactions/send-money.js";
 import { Wallet } from "./wallet.js";
 import { KeyEncryption } from "./key-encryption.js"
+import { CryptoUtil } from "./crypto-util.js";
 
 export {
-    Wallet, Provider, Signer, Transaction, SendMoney, KeyEncryption
+    Wallet, Provider, Signer, Transaction, SendMoney, KeyEncryption, CryptoUtil
 }
